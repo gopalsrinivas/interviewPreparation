c178c603090d01a06aad9f361aed8b6503bd8206
Gopal <gopalsrinivas.b@gmail.com>
2023-11-17 20:38:51 +0530

    add new question and ans

---
+++ problems.py
@@ -0,0 +1,10 @@
+# string:
+# -------
+Python Slicing Strings
+var = "Welcome To TutorialsPoint"
+print(type(var))
+------------------------
+1. String is an ordered sequence of Unicode characters
+var = "Welcome To TutorialsPoint"
+print(type(var))
+
